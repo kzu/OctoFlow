cdf09c8cbee0f447013d8ef09371c4f2bae61ab7
Daniel Cazzulino <daniel@cazzulino.com>
2014-07-13 01:58:03 -0300

    Renamed to OctoFlow

---
+++ src/OctoFlow/ProcessFlow.cs
@@ -0,0 +1,22 @@
+﻿/*
+   Copyright 2014 Daniel Cazzulino
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+namespace OctoFlow
+{
+    public class ProcessFlow
+    {
+        public ProcessFlow(ProcessType type)
+        {
+            this.Type = type;
+        }
+
+        public ProcessType Type { get; private set; }
+    }
+}
---
+++ src/OctoFlow/ProcessType.cs
@@ -0,0 +1,23 @@
+﻿/*
+   Copyright 2014 Daniel Cazzulino
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+*/
+namespace OctoFlow
+{
+    using System.ComponentModel;
+
+    public enum ProcessType
+    {
+        [Description("Development")]
+        Dev,
+        [Description("QA")]
+        QA,
+        [Description("Documentation")]
+        Doc,
+    }
+}
