8176841780925a45014cc57d1138239ee7dea062
Daniel Cazzulino <daniel@cazzulino.com>
2014-07-14 00:18:33 -0300

    Added simplified console messages

---
+++ src/OctoFlow.Console/SimpleConsoleListener.cs
@@ -0,0 +1,38 @@
+﻿/*
+   Copyright 2014 Daniel Cazzulino
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+namespace OctoFlow
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+
+    public class SimpleConsoleListener : ConsoleTraceListener
+    {
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
+        {
+            if ((this.Filter == null) || this.Filter.ShouldTrace(eventCache, source, eventType, id, message, null, null, null))
+            {
+                this.WriteLine(message);
+            }
+        }
+
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string format, params object[] args)
+        {
+            if ((this.Filter == null) || this.Filter.ShouldTrace(eventCache, source, eventType, id, format, args, null, null))
+            {
+                if (args != null)
+                    this.WriteLine(string.Format(CultureInfo.InvariantCulture, format, args));
+                else
+                    this.WriteLine(format);
+            }
+        }
+    }
+}
