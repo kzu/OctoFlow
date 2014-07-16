641e89b28aa538e5ed7069a4c8593aa2cadeed65
Daniel Cazzulino <daniel@cazzulino.com>
2014-07-16 01:27:39 -0300

    Improve error handling

--- src/OctoFlow.Console/Program.cs
+++ src/OctoFlow.Console/Program.cs
@@ -10,23 +10,50 @@
 
 namespace OctoFlow
 {
-	using CLAP;
+    using CLAP;
     using OctoFlow.Diagnostics;
     using System;
     using System.Diagnostics;
     using System.Linq;
+    using System.Threading.Tasks;
 
     class Program
-	{
-		static void Main(string[] args)
-		{
+    {
+        static void Main(string[] args)
+        {
             Tracer.Initialize(new TracerManager());
 
-            Parser.Run(args, new App());
+            AppDomain.CurrentDomain.UnhandledException += OnDomainUnhandledException;
+            System.Threading.Tasks.TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
+
+            var parser = new Parser<App>();
+            parser.Register.ErrorHandler(e => Console.WriteLine(e.Exception.Message));
+
+            parser.Run(args, new App());
+
+            //Parser.Run(args, new App());
 
 #if DEBUG
             Console.ReadLine();
 #endif
-		}
-	}
+        }
+
+        private static void OnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Tracer.Get(typeof(Program)).Error(e.Exception.Flatten(), "Unexpected exception");
+            Console.WriteLine(e.Exception.Flatten().ToString());
+            Console.WriteLine("Press [Enter] to exit.");
+            Console.ReadLine();
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private static void OnDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Tracer.Get(typeof(Program)).Error((Exception)e.ExceptionObject, "Unexpected exception");
+            Console.WriteLine(e.ExceptionObject.ToString());
+            Console.WriteLine("Press [Enter] to exit.");
+            Console.ReadLine();
+            Process.GetCurrentProcess().Kill();
+        }
+    }
 }
